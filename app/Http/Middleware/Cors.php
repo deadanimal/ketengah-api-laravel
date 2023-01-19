dc9b20f9e255d6437a7008c592a05ceaa04c20c2
Jagdish Patel <jagdish.j.ptl@gmail.com>
2023-01-19 17:09:37 +0530

    Update Cors.php

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,6 +17,9 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
         return $next($request)
-        ->header('Access-Control-Allow-Origin', '127.0.0.1:8100');
+        ->header('Access-Control-Allow-Origin', '127.0.0.1:8100')
+        ->header('Access-Control-Allow-Origin', 'localhost:8100')
+        ->header('Access-Control-Allow-Origin', 'http://localhost:8100')
+        ->header('Access-Control-Allow-Origin', 'http://127.0.0.1:8100');
     }
 }
