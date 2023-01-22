e2d8d6df430a3aff23dcebf32178d8f0f2a374bc
Jagdish Patel <jagdish.j.ptl@gmail.com>
2023-01-22 11:39:54 +0530

    Update Cors.php

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,13 +16,11 @@ class Cors
      */
     public function handle(Request $request, Closure $next)
     {
-        $response = $next($request)
-        /* ->header('X-Frame-Options',  'allow from *') */
+        return $next($request)
+        ->header('X-Frame-Options',  'allow from *')
         ->header('Access-Control-Allow-Origin', '127.0.0.1:8100')
         ->header('Access-Control-Allow-Origin', 'localhost:8100')
         ->header('Access-Control-Allow-Origin', 'http://localhost:8100')
         ->header('Access-Control-Allow-Origin', 'http://127.0.0.1:8100');
-
-        return $response->headers->set('X-Frame-Options', 'ALLOW-FROM *', false);
     }
 }
